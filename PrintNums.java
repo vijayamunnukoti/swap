ab31dd54af802bfeee35db8bd04cfd5dd176bc95
vijayamunnukoti <134375817+vijayamunnukoti@users.noreply.github.com>
2023-06-08 14:51:10 +0530

    Add files via upload

---
+++ PrintNums.java
@@ -0,0 +1,12 @@
+package printnums;
+
+public class PrintNums {
+	
+public static void main(String[]args) {
+	int i;
+	for(i=0;i<=10;i++) {
+		System.out.println("the sequence of num is "+i);
+	}
+	
+}}
+
