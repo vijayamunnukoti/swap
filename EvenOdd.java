6ee5b38dc526df3cd80704a8af27100af54c338b
vijayamunnukoti <134375817+vijayamunnukoti@users.noreply.github.com>
2023-06-08 14:25:49 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,26 @@
+package evenodd;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			
+				int n=353;
+				if(n%2==0)
+				{
+					System.out.println("num is even  n "+ n );
+					
+				}
+				else if(n%2!=0)
+				{
+					System.out.println("num is odd "+ n);
+				}
+				
+				}
+		
+
+
+
+	}
+
